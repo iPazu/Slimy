330bcf475949112d25d03e0cb32003f2d79f0c8d
Alex <50049847+iPazu@users.noreply.github.com>
2020-06-09 15:32:23 +0200

    removed playsound

--- entity.py
+++ entity.py
@@ -5,7 +5,6 @@ from direct.showbase.ShowBase import ShowBase
 #Util import
 from math import sqrt
 import platform
-from playsound import playsound
 
 def distance(A, B):
     return round(sqrt( (A[0]-B[0])**2 + (A[1]-B[1])**2 + (A[2]-B[2])**2 ))
