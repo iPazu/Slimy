f1ac3ea046d305c707ebf2111763e678aafb8fb3
pjoly <pierre.joly@ecl21.ec-lyon.fr>
2023-04-22 13:51:39 +0200

    Restore game functionality

--- database.py
+++ database.py
@@ -1,43 +1,51 @@
-import mysql.connector
+import sqlite3
 
 class Database:
     def __init__(self):
-        self.db = mysql.connector.connect(
-        host="51.210.96.48",
-        user="root",
-        passwd="slimydb",
-        database="slimy")
+        self.db = sqlite3.connect('slimy.db')
+        self.create_classement_table()
 
-    def insertValues(self,name,score,date):
-        mySql_insert_query = """INSERT INTO classement (nom, score, date) 
-                           VALUES (%s, %s, %s) """
-        val = (name, score,date)
+    def create_classement_table(self):
+        cursor = self.db.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS classement (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nom TEXT NOT NULL,
+                score INTEGER NOT NULL,
+                date TEXT NOT NULL
+            )
+        """)
+        self.db.commit()
+
+    def insertValues(self, name, score, date):
+        sqlite_insert_query = """INSERT INTO classement (nom, score, date) 
+                                 VALUES (?, ?, ?)"""
+        val = (name, score, date)
 
         cursor = self.db.cursor()
-        cursor.execute(mySql_insert_query,val)
+        cursor.execute(sqlite_insert_query, val)
 
         self.db.commit()
         print(cursor.rowcount, "Successfully sent values to database")
         cursor.close()
-    
+
     def getRankingFromDatabase(self):
         cursor = self.db.cursor()
-        mySql_select_query = """SELECT nom,max(score) FROM classement
-                           GROUP BY nom
-                           ORDER BY max(score) DESC
-                           LIMIT 4
-                           """
+        sqlite_select_query = """SELECT nom, max(score) FROM classement
+                                 GROUP BY nom
+                                 ORDER BY max(score) DESC
+                                 LIMIT 4
+                              """
 
-        cursor.execute(mySql_select_query)
+        cursor.execute(sqlite_select_query)
         firstresult = cursor.fetchall()
         finalresult = []
         for r in firstresult:
-            sql_date_query = """SELECT date FROM classement
-                                WHERE nom = "{}" AND score = {}
-                                """.format(r[0],r[1])
-            cursor.execute(sql_date_query)
+            sqlite_date_query = """SELECT date FROM classement
+                                   WHERE nom = ? AND score = ?
+                                """
+            cursor.execute(sqlite_date_query, (r[0], r[1]))
             result = cursor.fetchall()
-            finalresult.append([r[0],r[1],result[0]])
-
+            finalresult.append([r[0], r[1], result[0]])
 
         return finalresult
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Panda3d import
+# Panda3d import
 from panda3d.core import *
 from panda3d.ai import *
 from panda3d.physics import *
@@ -8,7 +8,7 @@ from direct.actor.Actor import Actor
 from direct.showbase.ShowBase import ShowBase
 
 
-#Util import
+# Util import
 import os
 import sys
 import getpass
@@ -16,7 +16,7 @@ from datetime import datetime as date
 import ctypes
 
 
-#Class imports
+# Class imports
 from gui.menu import Menu
 from gui.hud import Hud
 from gui.gameover import Gameover
@@ -31,42 +31,34 @@ from skybox import Skybox
 from monster import Monster
 from slime import Slime
 
-user32 = ctypes.windll.user32
-user32.SetProcessDPIAware() #windows fullscreen compatibility, fixes the getsystemmetrics bug
-fullscreen=False
-if fullscreen:
-    loadPrcFileData('', 'fullscreen true') 
-    loadPrcFileData('','win-size '+str(user32.GetSystemMetrics(0))+' '+str(user32.GetSystemMetrics(1))) # fullscreen stuff for one monitor, for multi monitor setup try 78 79
+
 class MyApp(ShowBase):
 
     def __init__(self):
         self.debug = False
         ShowBase.__init__(self)
-        self.accept("escape",sys.exit)
+        self.accept("escape", sys.exit)
 
         # the dt should depend on the framerate
         self.dt = 0.25
-        """try:
-            self.database = Database()
-            self.ranking = self.database.getRankingFromDatabase()
-        except:
-            pass"""
+
+        # Make ranking
+        self.database = Database()
+        self.ranking = self.database.getRankingFromDatabase()
+        self.classement = Classement(self.ranking)
+        self.classement.hideMenu()
+
         self.musicManager.setConcurrentSoundLimit(2)
 
-        #initiate game state
+        # initiate game state
         self.state = 'Menu'
         self.terrain = Terrain(1024)
-        """try:
-            self.classement = Classement(self.ranking) 
-            self.classement.hideMenu()
-        except:
-            pass"""
         self.menu = Menu()
         self.loadStartMenu()
 
-        if(self.debug == False):
+        if (self.debug == False):
             self.disableMouse()
-        
+
     def loadStartMenu(self):
         self.accept("Menu-Start-Parkour", self.loadGame)
         self.accept("Menu-Start-World", self.loadGame)
@@ -81,16 +73,16 @@ class MyApp(ShowBase):
         self.ignore("Menu-Start-Parkour")
         self.menu.hideStartMenu()
 
-    def loadGameOverMenu(self,score):
+    def loadGameOverMenu(self, score):
         self.gameover = Gameover(score)
-        taskMgr.doMethodLater(5, self.restartGame,'timer')
+        taskMgr.doMethodLater(5, self.restartGame, 'timer')
         self.show_cursor()
 
     def loadRankingMenu(self):
         self.accept("Menu-Ranking-Return", self.exitRankingMenu)
         self.exitStartMenu()
         self.classement.showMenu()
-        
+
         self.show_cursor()
 
     def exitRankingMenu(self):
@@ -98,13 +90,13 @@ class MyApp(ShowBase):
         self.ignore("Menu-Ranking-Return")
         self.classement.hideMenu()
         self.loadStartMenu()
-        
+
     def loadGame(self):
         self.exitStartMenu()
         print("Loading game")
         self.state = 'Loading'
-        
-        if(self.debug == False):
+
+        if (self.debug == False):
             self.hide_cursor
         self.setLights()
 
@@ -113,35 +105,36 @@ class MyApp(ShowBase):
         self.hud.show()
         self.loadEntities()
 
-        #positionate the camera
-        if(self.debug == False):
+        # positionate the camera
+        if (self.debug == False):
             self.camera.lookAt(self.slime.model)
         # Load Skybox
         Skybox(self.render)
 
-        #register events
+        # register events
         self.ydelta = 300
         self.zdelta = 60
-        self.accept("wheel_up", self.camzoom,[True])
-        self.accept("wheel_down", self.camzoom,[False])
+        self.accept("wheel_up", self.camzoom, [True])
+        self.accept("wheel_down", self.camzoom, [False])
 
-        #register tasks
+        # register tasks
         self.task_mgr.add(self.mainLoop, "MainTask")
-        if(self.debug == False):
+        if (self.debug == False):
             self.task_mgr.add(self.updateCamera, "CameraTask")
         self.startGame()
 
     def startGame(self):
         print("Starting game")
-        #Load music
-        self.music = base.loader.loadMusic("assets/sounds/hytale-ost-kweebec-village.mp3")
+        # Load music
+        self.music = base.loader.loadMusic(
+            "assets/sounds/hytale-ost-kweebec-village.mp3")
         self.music.play()
         self.music.setVolume(0.1)
 
         self.setFrameRateMeter(True)
         self.state = 'Game'
-        
-        #init console
+
+        # init console
         """
         self.userConsole = pc.Console()
         commands = {"restart":self.__init__,
@@ -151,7 +144,7 @@ class MyApp(ShowBase):
                     }
         self.userConsole.create(commands,app=self)
         """
-        
+
     def endGame(self):
         self.state = 'Finished'
         print("SCORE : "+str(Monster.score))
@@ -159,21 +152,23 @@ class MyApp(ShowBase):
         self.loadGameOverMenu(Monster.score)
         today = date.today()
         name = getpass.getuser()
-        """try:
+        try:
             self.database.insertValues(name.capitalize(),Monster.score,today.strftime("%d/%m/%Y"))
         except:
-            pass"""
+            pass
 
-    def restartGame(self,task):
+    def restartGame(self, task):
         os.execl(sys.executable, sys.executable, *sys.argv)
 
     def loadEntities(self):
         startingPoint = (100, 0, 10)
         self.AIworld = AIWorld(render)
         self.collision = Collision(Monster.monster)
-        #terrain, initialPos, slimeModelPath, floorPos, scale, lifePoint, volumicMass, movingSpeed, dt
-        self.slime = Slime(self.terrain, startingPoint, "assets/models/slime.egg", 10, 10, 100, 0.01, 5, self.dt, "slime", self.collision) 
-        self.spawn = Spawn([self.slime]+Monster.monster, self.terrain, self.AIworld, self.collision)
+        # terrain, initialPos, slimeModelPath, floorPos, scale, lifePoint, volumicMass, movingSpeed, dt
+        self.slime = Slime(self.terrain, startingPoint, "assets/models/slime.egg",
+                           10, 10, 100, 0.01, 5, self.dt, "slime", self.collision)
+        self.spawn = Spawn([self.slime]+Monster.monster,
+                           self.terrain, self.AIworld, self.collision)
         self.spawn.spawn()
 
     def setLights(self):
@@ -182,18 +177,18 @@ class MyApp(ShowBase):
         sun.setScene(render)
         self.sunNp = render.attachNewNode(sun)
         self.sunNp.setPos(-10, -10, 30)
-        self.sunNp.lookAt(0,0,0)
+        self.sunNp.lookAt(0, 0, 0)
         render.setLight(self.sunNp)
-    
+
         alight = AmbientLight('alight')
         alight.setColor((0.35, 0.35, 0.35, 1))
         alnp = render.attachNewNode(alight)
         render.setLight(alnp)
 
-    def mainLoop(self,task):
-        if(self.state == "Finished"):
+    def mainLoop(self, task):
+        if (self.state == "Finished"):
             return
-        if(self.slime.lifePoint <= 0 or self.slime.scale >= 1000):
+        if (self.slime.lifePoint <= 0 or self.slime.scale >= 1000):
             self.endGame()
         self.AIworld.update()
         self.spawn.spawn()
@@ -203,23 +198,22 @@ class MyApp(ShowBase):
         self.hud.setScore(Monster.score)
         return task.cont
 
-    def camzoom(self,decrease):
-        if(decrease):
-            self.ydelta-=5
-            self.zdelta-=1
+    def camzoom(self, decrease):
+        if (decrease):
+            self.ydelta -= 5
+            self.zdelta -= 1
         else:
-            self.ydelta+=5
-            self.zdelta+=1
+            self.ydelta += 5
+            self.zdelta += 1
 
-    def updateCamera(self,task):
-        if(self.state != 'Game'):
+    def updateCamera(self, task):
+        if (self.state != 'Game'):
             return
-        self.cam.setPos(self.slime.pos.getX(),self.slime.pos.getY()-self.ydelta,self.slime.pos.getZ()+self.zdelta)
-        #print("x:"+str(self.camera.getX()-self.slime.pos.getX())+" y:"+str(self.camera.getY()-self.slime.pos.getY())+" z:"+str(self.camera.getZ()))
-        #print(self.cam.getHpr())
+        self.cam.setPos(self.slime.pos.getX(), self.slime.pos.getY(
+        )-self.ydelta, self.slime.pos.getZ()+self.zdelta)
         self.cam.lookAt(self.slime.model)
         return task.cont
-    
+
     def hide_cursor(self):
         props = WindowProperties()
         props.setCursorHidden(True)
@@ -228,9 +222,9 @@ class MyApp(ShowBase):
     def show_cursor(self):
         """set the Cursor visible again"""
         props = WindowProperties()
-        props.setCursorHidden(False)        
+        props.setCursorHidden(False)
         self.win.requestProperties(props)
-    
+
 
 App = MyApp()
 App.run()
